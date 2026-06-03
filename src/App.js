9a9fd0e863c42d1fa00865fbbaf31308ea76d675
Sollarium22 <126174990+Sollarium22@users.noreply.github.com>
2026-06-03 10:14:27 -0300

    adicionando propriedades construcoes
    adicionando construcoes valores preços e icones

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 import dado from './d4.png'
+import dodo from './dodo.png'
 
 
 function App() {
@@ -10,9 +11,9 @@ function App() {
   const [DPS, setDps] = useState(0); // total de Dados por segundo Dps
   const [preco, setPreco] = useState(15);// preco
   const [construcoes, setConstrucoes] = useState([
-  {nome: "dado", preco: 15, dps: 0.2,  quantidade: 0},
-  {nome: "jogador", preco: 100,dps: 1,  quantidade: 0},
-  {nome: "mestre", preco: 1000,dps: 5,  quantidade: 0}
+  {nome: "dado", preco: 15, dps: 0.2,  quantidade: 0, icone: dodo},
+  {nome: "jogador", preco: 100,dps: 1,  quantidade: 0, icone: dodo},
+  {nome: "mestre", preco: 1000,dps: 5,  quantidade: 0, icone: dodo}
   ]
  
   )
@@ -24,10 +25,12 @@ function App() {
 
   useEffect(() =>{
     const timer = setInterval(() =>{
-      setContagem((atual) => atual + DPS)
-    }, 1000); //a cada 1 segundo roda aqui
-    return() => clearInterval(timer);
-  }, [DPS]);
+      const producao = construcoes.reduce((soma, c) => soma + c.dps * c.quantidade, 0)
+      setDps(producao);
+      setContagem((atual) => atual + producao/10);
+    }, 100); //a cada 1 segundo roda aqui
+    return() => clearInterval(timer);//limpa o timer
+  }, [construcoes]);
 
   function comprarConstrucao(indice) {
     setConstrucoes((anterior) =>
@@ -37,13 +40,13 @@ function App() {
           return  {
             ...c,
             quantidade: c.quantidade+1,
-            preco: Math.floor(preco*1.2)
+            preco: Math.floor(c.preco*1.2)
           }
         }
         return c;
       }
       )
-    )
+    )  
   }
 
 
@@ -52,8 +55,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Dado Clicker</h1>
-        <h2>Total de numeros: {contagem}</h2>
-        <h3>DPS: {DPS}</h3>
+        <h2>Total de numeros: {Math.floor(contagem)}</h2>
+        <h3>DPS: {DPS.toFixed(1)}</h3>
         <button id="dado" onClick={contarDado}><img src={dado} className="App-logo" alt="logo"style={{cursor: "pointer"}}/> </button>
          
       </header>
@@ -62,8 +65,7 @@ function App() {
         <button id="nerd" onClick={comprarConstrucao} style={{cursor: "pointer"}}>Nerd -  preço: {preco}</button>
         
     {construcoes.map((c, i) => 
-      <button> Comprar {c.nome}
-      </button>
+      <button onClick={() => comprarConstrucao(i)}> <img src={c.icone}></img> {c.nome} preço: {c.preco} Quantidade: {c.quantidade} </button>
     )}
     
     </div>
